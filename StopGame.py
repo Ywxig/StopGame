df32d6a8737f625893aae5553d653598ea53fff1
Ywxig <82500877+Ywxig@users.noreply.github.com>
2023-07-03 14:46:56 +0300

    Update StopGame.py

--- StopGame.py
+++ StopGame.py
@@ -8,7 +8,7 @@ import random
 def game1():
     URL = 'https://stopgame.ru/games/Android'
     HEDERS = {
-            'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+            'User-Agent' : "YOUR USER AGENT"
         }
 
     response = requests.get(URL, HEDERS)
